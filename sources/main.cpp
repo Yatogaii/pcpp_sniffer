b0a85f82d2228e6239252b43333110a12e4bbb61
ykh <781023956@qq.com>
2023-10-12 18:55:47 +0800

    用emacs 了，换成中文注释

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,30 +4,31 @@
 #include <pcap/pcap.h>
 #include <string>
 #include <thread>
+#include <memory>
 
-
-/// packet handle function
-/// usage: 
-///   registered by pcap_loop(handle,int,`packetHandler`,nullptr)
+/// 处理 packet 的函数
+/// usage:
+///   通过 pcap_loop(handle,int,`packetHandler`,nullptr) 注册
 void packetHandler(u_char *userData, const struct pcap_pkthdr *pkthdr,
                    const u_char *packet) {
   std::cout << "Packet captured at: "
             << std::ctime((const time_t *)&pkthdr->ts.tv_sec);
 }
 
-/// subthread used to handle packet,
-/// for not block main thread
-void packetHandleThread(pcap_t* handle){
+
+/// 用于处理 packet 的子线程
+/// 防止阻塞主线程
+void packetHandleThread(pcap_t *handle) {
   pcap_loop(handle, 100, packetHandler, nullptr);
 }
 
-void initSniffer(std::string ifname){
-
+std::unique_ptr<pcap_t> initSniffer(std::string ifname) {
+  return nullptr;
 }
 
 int main() {
   char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_t* handle;
+  pcap_t *handle;
 
   handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
   if (handle == nullptr) {
@@ -35,13 +36,15 @@ int main() {
     return 1;
   }
 
-    std::thread captureThread(packetHandleThread, handle);
+  std::thread captureThread(packetHandleThread, handle);
 
-    // 在此处，主线程可以执行其他任务
-    // ...
 
-    // 等待捕获线程结束
-    captureThread.join();
+
+  // 在此处，主线程可以执行其他任务
+  // ...
+
+  // 等待捕获线程结束
+  captureThread.join();
 
   pcap_close(handle);
 
